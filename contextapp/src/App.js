5c21181f7673d26f98825f226b956bac3a688a9e
Luke Russell <thelukerussel21@gmail.com>
2020-04-29 12:11:31 -0400

    added multiple contexts

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import ThemeContextProvider from './context/ThemeContext';
+import AuthContextProvider from './context/AuthContext';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
     <div className='App'>
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
